16dbbd7c0280340b9d6b073090fb1cebc3e0f4f4
vivekkumarsarswat060-a11y <vivekkumarsarswat060@gmail.com>
2026-07-16 11:35:40 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,42 @@
+const products = [
+  {
+    id: 1,
+    name: "Arhar Dal",
+    category: "Dal",
+    price250: 55,
+    price500: 110,
+    price1000: 220
+  },
+  {
+    id: 2,
+    name: "Moong Dal",
+    category: "Dal",
+    price250: 45,
+    price500: 90,
+    price1000: 180
+  },
+  {
+    id: 3,
+    name: "Haldi Powder",
+    category: "Spices",
+    price250: 70,
+    price500: 140,
+    price1000: 280
+  },
+  {
+    id: 4,
+    name: "Lal Mirch Powder",
+    category: "Spices",
+    price250: 90,
+    price500: 180,
+    price1000: 360
+  },
+  {
+    id: 5,
+    name: "Gehu Ka Aata",
+    category: "Flour",
+    price10kg:320
+  
+  
+  }
+];
