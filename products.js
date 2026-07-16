c6fecd13494c0056ebc5deb9aacc91c37e8cbb65
vivekkumarsarswat060-a11y <vivekkumarsarswat060@gmail.com>
2026-07-16 23:00:05 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 1,
+    name: "Arhar Dal",
+    category: "Dal",
+    sizes: [
+      { size: "500g", price: 110 },
+      { size: "1kg", price: 220 }
+    ]
+  },
+  {
+    id: 2,
+    name: "Moong Dal",
+    category: "Dal",
+    sizes: [
+      { size: "500g", price: 90 },
+      { size: "1kg", price: 180 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Masoor Dal",
+    category: "Dal",
+    sizes: [
+      { size: "500g", price: 80 },
+      { size: "1kg", price: 160 }
+    ]
+  },
+  {
+    id: 4,
+    name: "Chana Dal",
+    category: "Dal",
+    sizes: [
+      { size: "500g", price: 76 },
+      { size: "1kg", price: 152 }
+    ]
+  },
+  {
+    id: 5,
+    name: "Urad Dal",
+    category: "Dal",
+    sizes: [
+      { size: "500g", price: 100 },
+      { size: "1kg", price: 200 }
+    ]
+  }
+];
