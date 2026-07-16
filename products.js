c563e6cfd9de7a6a965ef9f1a049790ff86fa283
vivekkumarsarswat060-a11y <vivekkumarsarswat060@gmail.com>
2026-07-16 19:18:15 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,97 @@
+const products = [
+
+  ];
+// =====================
+// DAL (1-10)
+// =====================
+
+{
+id:1,
+name:"Arhar Dal",
+category:"Dal",
+sizes:[
+{size:"500g",price:110},
+{size:"1kg",price:220}
+]
+},
+{
+id:2,
+name:"Moong Dal",
+category:"Dal",
+sizes:[
+{size:"500g",price:90},
+{size:"1kg",price:180}
+]
+},
+{
+id:3,
+name:"Masoor Dal",
+category:"Dal",
+sizes:[
+{size:"500g",price:80},
+{size:"1kg",price:160}
+]
+},
+{
+id:4,
+name:"Chana Dal",
+category:"Dal",
+sizes:[
+{size:"500g",price:76},
+{size:"1kg",price:152}
+]
+},
+{
+id:5,
+name:"Urad Dal",
+category:"Dal",
+sizes:[
+{size:"500g",price:100},
+{size:"1kg",price:200}
+]
+},
+{
+id:6,
+name:"Kala Chana",
+category:"Dal",
+sizes:[
+{size:"500g",price:65},
+{size:"1kg",price:130}
+]
+},
+{
+id:7,
+name:"Kabuli Chana",
+category:"Dal",
+sizes:[
+{size:"500g",price:80},
+{size:"1kg",price:160}
+]
+},
+{
+id:8,
+name:"Rajma Chitra",
+category:"Dal",
+sizes:[
+{size:"500g",price:85},
+{size:"1kg",price:170}
+]
+},
+{
+id:9,
+name:"Rajma Lal",
+category:"Dal",
+sizes:[
+{size:"500g",price:90},
+{size:"1kg",price:180}
+]
+},
+{
+id:10,
+name:"Lobiya",
+category:"Dal",
+sizes:[
+{size:"500g",price:75},
+{size:"1kg",price:150}
+]
+},
