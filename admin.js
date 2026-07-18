88d3b85a75ed300fb9e7b1d97f434cd65dd583db
vivekkumarsarswat060-a11y <vivekkumarsarswat060@gmail.com>
2026-07-18 08:04:39 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -25,6 +25,55 @@ function addProduct() {
         alert("Please fill all fields.");
         return;
     }
+    let products = [];
+
+function addProduct() {
+    const name = document.getElementById("productName").value;
+    const category = document.getElementById("productCategory").value;
+    const price = document.getElementById("productPrice").value;
+
+    if (!name || !category || !price) {
+        alert("Please fill all fields.");
+        return;
+    }
+
+    products.push({
+        name,
+        category,
+        price
+    });
+
+    showProducts();
+
+    document.getElementById("productName").value = "";
+    document.getElementById("productCategory").value = "";
+    document.getElementById("productPrice").value = "";
+}
+
+function showProducts() {
+
+    const list = document.getElementById("productList");
+
+    if(products.length===0){
+        list.innerHTML="No Products Added";
+        return;
+    }
+
+    list.innerHTML="";
+
+    products.forEach((item,index)=>{
+
+        list.innerHTML += `
+        <div style="background:#555;padding:10px;border-radius:8px;margin:10px 0;">
+            <b>${item.name}</b><br>
+            ${item.category}<br>
+            ₹${item.price}
+        </div>
+        `;
+
+    });
+
+        }
 
     alert("Product Added Successfully!\n\n" +
           "Name: " + name +
